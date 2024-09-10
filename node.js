1e45633e47f39d1f9ffcf189ea2681e640bd26b7
pabllwn <92005758+pabllwn@users.noreply.github.com>
2024-09-10 23:31:08 +0100

    Update node.js

--- node.js
+++ node.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const { Client } = require('discord.js-selfbot-v13');
-const mySecret = 'ODI2NTcxNDY2ODE1NTY5OTcw.GlIMOJ.UxygMkYr1lb1MJ0hg9ZWHZrSwLsoXkdknfuik0'; // التوكن الشخصي
+const mySecret = 'ODg1MTc5NTgyOTA1NDU0Njcy.Gw_5lw.MBkYZnyd_DeEtA3Ka2fh6yGnferygU4tVJs284'; // التوكن الشخصي
 
 const client = new Client();
 
@@ -14,9 +14,9 @@ client.on("messageCreate", message => {
     console.log("Received a message");
 
     // تحقق من معرفات القنوات الصحيحة
-    const channel = client.channels.cache.get('1123064802361753632'); // قناة ROB
-    const channel1 = client.channels.cache.get('1123064802361753632'); // قناة FIN TATl3b
-    const rob = client.channels.cache.get('1123064802361753632'); // قناة CHAT FIN trobi
+    const channel = client.channels.cache.get('1037831289585291264'); // قناة ROB
+    const channel1 = client.channels.cache.get('1037831299592880209'); // قناة FIN TATl3b
+    const rob = client.channels.cache.get('1047751275665698867'); // قناة CHAT FIN trobi
 
     if (flag) return;
 
@@ -24,25 +24,25 @@ client.on("messageCreate", message => {
         flag = true;
         console.log("Executing commands");
 
-        channel.send('!rob 1076271692424433774').then(() => console.log('Sent !rob command'));
+        channel.send('!rob 833869005500448808').then(() => console.log('Sent !rob command'));
         setTimeout(() => {
             channel1.send('!bal').then(() => console.log('Sent !bal command'));
-        }, 4000);
+        }, 300);
         setTimeout(() => {
             channel1.send('!buy K').then(() => console.log('Sent !buy command'));
-        }, 6500);
+        }, 200);
         setTimeout(() => {
             channel1.send('!with 500').then(() => console.log('Sent !with 500 command'));
-        }, 8000);
+        }, 600);
         setTimeout(() => {
             channel1.send('!cf 100').then(() => console.log('Sent !cf 100 command'));
-        }, 9000);
+        }, 300);
         setTimeout(() => {
             channel1.send('!dep all').then(() => console.log('Sent !dep all command'));
-        }, 10000);
+        }, 900);
         setTimeout(() => {
             rob.send('!dep all').then(() => console.log('Sent !dep all command for rob'));
-        }, 12000);
+        }, 200);
     }
 });
 
